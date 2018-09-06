afbef2be83c17a1bac1409e11c9f85c2b6df9b0f
Philip James <pjj@philipjohnjames.com>
2018-09-06 14:52:30 -0700

    Rework things to support sub-events

--- schedule.py
+++ schedule.py
@@ -36,18 +36,71 @@ def find_events(schedule, current_day, check_time, window):
     current = datetime.datetime.now(tz=tz)
     if check_time:
         hour, minute = check_time.split(':')
-        current = current.replace(hour=8, minute=55)
+        current = current.replace(hour=int(hour), minute=int(minute))
     for event in schedule:
         if current_day in event['days']:
             current_day_event = event['days'][current_day]
-            start_time = datetime.datetime.strptime(current_day_event['start'].upper(), "%I%p")
-            start_time = current.replace(hour=start_time.hour, minute=start_time.minute)
-            if current < start_time and start_time < (current + datetime.timedelta(minutes=window)):
-                toot_string = f"{event['name']} is starting at {current_day_event['start']}!\n\nVenue: {event['venue'].title()}\n\n{current_day_event['description']}"
-                if len(toot_string) >= 500:
-                    toot_string = toot_string[:490] + '...'
-                events_to_toot.append(toot_string)
+            events_to_toot.append(
+                parse_event(
+                    current_day_event,
+                    current,
+                    window,
+                    event['name'],
+                    event['venue'],
+                    current_day_event.get('start', event.get('start')),
+                    '',
+                ),
+            )
+            if event.get('lineup'):
+                for lineup_event in event['lineup']:
+                    venue = lineup_event.get('location', event.get('venue'))
+                    start_time = lineup_event.get('start')
+                    if start_time:
+                        events_to_toot.append(
+                            parse_event(
+                                lineup_event,
+                                current,
+                                window,
+                                lineup_event['name'],
+                                venue,
+                                start_time,
+                                '',
+                            )
+                        )
+            if event.get('lineup', [{}])[0].get('subevents'):
+                for lineup_event in event['lineup'][0]['subevents']:
+                    venue = lineup_event.get('location', event.get('venue'))
+                    start_time = lineup_event.get('start', current_day_event.get('start'))
+                    events_to_toot.append(
+                        parse_event(
+                            lineup_event,
+                            current,
+                            window,
+                            lineup_event['name'],
+                            venue,
+                            start_time,
+                            " meetup",
+                        )
+                    )
+    events_to_toot = [toot for toot in events_to_toot if toot is not None]
     return events_to_toot
+
+def parse_event(event, current, window, name, venue, start_time, extra_string=None):
+    if ':' in start_time:
+        parsed_start_time = datetime.datetime.strptime(start_time.upper(), "%I:%M%p")
+    else:
+        parsed_start_time = datetime.datetime.strptime(start_time.upper(), "%I%p")
+    parsed_start_time = current.replace(
+        hour=parsed_start_time.hour,
+        minute=parsed_start_time.minute,
+    )
+    if event.get('location_url'):
+        venue = venue + ' ' + event['location_url']
+    if current < parsed_start_time and parsed_start_time < (current + datetime.timedelta(minutes=window)):
+        toot_string = f"{name}{extra_string} is starting at {start_time}!\n\nVenue: {venue}\n\n{event.get('description', '✨')}"
+        if len(toot_string) >= 500:
+            toot_string = toot_string[:490] + '...'
+        return toot_string
 
     
 if __name__ == '__main__':
